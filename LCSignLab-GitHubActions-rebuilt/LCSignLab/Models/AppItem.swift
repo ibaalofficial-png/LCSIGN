65c1874fdbf877309b6d2299149071cda4e7c0ce
ibaalofficial-png <ibaalofficial@gmail.com>
2026-09-13 02:17:51 +0700

    Modified by www.SourceFiles.app

--- LCSignLab-GitHubActions-rebuilt/LCSignLab/Models/AppItem.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-struct AppItem: Identifiable, Hashable {
-    let id = UUID()
-    let name: String
-    let bundleIdentifier: String
-    let version: String
-    let sourceURL: URL?
-}
